2b4af2cae14aeaa339ff80e93faecdf3c4afa7a8
jiara <ji.arakani@gmail.com>
2022-05-31 09:59:43 -0400

    first commit

---
+++ testing123.js
@@ -0,0 +1,6 @@
+// This function takes a list of strings and returns each line prepended by the correct number, similar to an object with numbered keys to values.
+const number = array => array.map((n, i) => `${i + 1}: ${n}`)
+
+number([])  // []
+number(["a", "b", "c", "d", "e"])  // ["1: a", "2: b", "3: c"]
+number(["", "", "", "", ""])  // ["1: ", "2: ", "3: ", "4: ", "5: "]
